9b22f22fffe6c9e1ca1f2c1783bf7c3311ab8e02
quasarbright <mthundershock@gmail.com>
2021-03-20 16:26:36 -0400

    ast

---
+++ src/AST.hs
@@ -0,0 +1,23 @@
+module AST where
+
+newtype Program = Program [Statement] deriving(Eq, Ord, Show)
+
+data Statement
+    = While Expr [Statement]
+    | If Expr [Statement] (Maybe [Statement])
+    | Assign String Expr
+    | Eval Expr
+    deriving(Eq, Ord, Show)
+
+data Binop = Plus | Minus | Times | FloorDiv | Or | And | Eq | Neq | Lt | Le | Gt | Ge deriving(Eq, Ord, Show)
+data Unop = Not deriving(Eq, Ord, Show)
+
+data Expr
+    = Var String
+    | Number Int
+    | String String
+    | Bool Bool
+    | Unop Unop Expr
+    | Binop Binop Expr Expr
+    | Call Expr [Expr]
+    deriving(Eq, Ord, Show)
