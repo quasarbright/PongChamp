988316f99aa830a861e72605df6ae7fc45b3f62d
RyanRio <thermitchellman@gmail.com>
2021-04-01 15:53:16 -0700

    import sys

--- examples/brain-oof.js
+++ examples/brain-oof.js
@@ -1,11 +1,21 @@
 function f(x) {
+	__printState__();
 	function g() {
 		x = x + 1;
 		return x;
 	}
 	return g;
+
 }
+
 
 let h = f(2);
 h();
 println(h()); // prints 4
+
+function a() {
+	__printState__();
+	a();
+}
+
+a();
